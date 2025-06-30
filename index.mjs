8c46d02c7ff79b9969e108d60009aad58b0b8a7f
Antonio <antonnio.llano@gmail.com>
2025-06-30 06:53:51 -0400

    Primer commit

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const PLACE_ID = "4499009264";
+
+app.get('/gamepasses', async (req, res) => {
+    try {
+        const url = `https://games.roblox.com/v1/games/${PLACE_ID}/game-passes`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            return res.status(500).json({ error: 'Error al obtener game passes' });
+        }
+        const data = await response.json();
+        res.json(data.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
